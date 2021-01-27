6a28c3e6780bc68944519bf8c4e25638391acd11
Frederik Dahl <cph-fd76@cphbusiness.dk>
2021-01-27 13:10:35 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Figure from "./components/Figure";
+import WrongLetters from "./components/WrongLetters";
+import Word from "./components/Word";
+import Notication from "./components/Notification";
+import Popup from "./components/Popup";
+import Image from "./components/Image";
+import { showNotification as show } from "./helpers/helpers";
+
+const words = [
+  "luke skywalker",
+  "anakin skywalker",
+  "fives",
+  "rex",
+  "obi-wan kenobi",
+];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
+function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  const [showNotification, setShowNotification] = useState(false);
+
+  useEffect(() => {
+    const handleKeydown = (event) => {
+      const { key, keyCode } = event;
+
+      if (playable && keyCode >= 65 && keyCode <= 90) {
+        const letter = key.toLowerCase();
+
+        if (selectedWord.includes(letter)) {
+          if (!correctLetters.includes(letter)) {
+            setCorrectLetters((correctLetters) => [...correctLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        } else {
+          if (!wrongLetters.includes(letter)) {
+            setWrongLetters((wrongLetters) => [...wrongLetters, letter]);
+          } else {
+            show(setShowNotification);
+          }
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeydown);
+
+    return () => window.removeEventListener("keydown", handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+
+  function playAgain() {
+    setPlayable(true);
+
+    //Empty arrays for data
+    setCorrectLetters([]);
+    setWrongLetters([]);
+
+    const random = Math.floor(Math.random() * words.length);
+    selectedWord = words[random];
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="game-container">
+        <div className="image-container">
+          <Image wrongLetters={wrongLetters} />
+        </div>
+        {/*<Figure wrongLetters={wrongLetters} /> */}
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
+      </div>
+      <Popup
+        correctLetters={correctLetters}
+        wrongLetters={wrongLetters}
+        selectedWord={selectedWord}
+        setPlayable={setPlayable}
+        playAgain={playAgain}
+      />
+      <Notication showNotification={showNotification} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Image.js
@@ -0,0 +1,45 @@
+import React from "react";
+
+import darthVader from "../img/darthvader_nobackground.jpeg";
+import img1 from "../img/1.png";
+import img2 from "../img/2.png";
+import img3 from "../img/3.png";
+import img4 from "../img/4.png";
+import img5 from "../img/5.png";
+import img6 from "../img/6.png";
+
+const Image = ({ wrongLetters }) => {
+  const errors = wrongLetters.length;
+
+  const styleObject = {
+    width: "100%",
+  };
+
+  return (
+    <div>
+      {errors === 0 && (
+        <img src={darthVader} alt="darthvader" style={styleObject}></img>
+      )}
+      {errors > 0 && errors < 2 && (
+        <img src={img1} alt="clone1" style={styleObject}></img>
+      )}
+      {errors > 1 && errors < 3 && (
+        <img src={img2} alt="clone2" style={styleObject}></img>
+      )}
+      {errors > 2 && errors < 4 && (
+        <img src={img3} alt="clone3" style={styleObject}></img>
+      )}
+      {errors > 3 && errors < 5 && (
+        <img src={img4} alt="clone4" style={styleObject}></img>
+      )}
+      {errors > 4 && errors < 6 && (
+        <img src={img5} alt="clone5" style={styleObject}></img>
+      )}
+      {errors > 5 && errors < 7 && (
+        <img src={img6} alt="clone6" style={styleObject}></img>
+      )}
+    </div>
+  );
+};
+
+export default Image;
